8c5c3eeeea78690e0455fa3f0aba5fa56659e55d
Shrish <shrishstark77@gmail.com>
2020-07-12 15:36:21 +0530

    player movement added with jumping collectible keys added

---
+++ Assets/scripts/KeyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Playercontroller>() != null)
+        {
+            Playercontroller playercontroller = collision.gameObject.GetComponent<Playercontroller>();
+            playercontroller.PickUpKey();
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/scripts/Levelovercontroler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelovercontroler : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<Playercontroller>() != null)
+        {
+            Debug.Log("level complete");
+        }
+    }
+}
